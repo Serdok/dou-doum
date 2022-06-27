3e2dbaf95290ef163564def5c759a9258d6e6633
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2022-06-27 17:32:32 +0200

    Update environment file targets

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -7,9 +7,7 @@ require('dotenv').config();
 const environment = argv.environment;
 const isProduction = environment === 'prod';
 
-const targetPath = isProduction
-  ? `./src/environments/environment.prod.ts`
-  : `./src/environments/environment.ts`
+const targetPath = `./src/environments/environment.ts`
 
 const environmentFileContents = `
 export const environment = {
