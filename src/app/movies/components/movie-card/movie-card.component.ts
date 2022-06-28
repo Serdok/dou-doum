4655067582c0ff68314d5fd4aade8fd38c81207f
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2022-06-29 00:41:45 +0200

    Changed routing style from function call to directive

--- src/app/movies/components/movie-card/movie-card.component.ts
+++ src/app/movies/components/movie-card/movie-card.component.ts
@@ -1,7 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Movie} from "../../../models/imdb/movie";
 import {ImdbImageService} from "../../../services/imdb/imdb-image.service";
-import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-movie-card',
@@ -12,14 +11,9 @@ export class MovieCardComponent implements OnInit {
   @Input('movie') movie!: Movie;
   error!: any;
 
-  constructor(public imdbImage: ImdbImageService, private router: Router, private route: ActivatedRoute) {
+  constructor(public imdbImage: ImdbImageService) {
   }
 
   ngOnInit(): void {
-  }
-
-  navigateTo(id: number): void {
-    // using a function rather than [routerLink] to explicitly set a starting point (relativeTo)
-    this.router.navigate(['movie', id], {relativeTo: this.route.parent,});
   }
 }
